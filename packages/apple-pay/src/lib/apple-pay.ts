db49fed560a933a361e961842da33aaebb856842
animesh1987 <animeshsingh1987@gmail.com>
2022-06-02 22:54:40 +1000

    chore(common): CHECKOUT-6377 Add apple pay package

---
+++ packages/apple-pay/src/lib/apple-pay.ts
@@ -0,0 +1,5 @@
+import { DefaultPaymentIntegrationService } from '@bigcommerce/checkout-sdk/payment-integration-core';
+
+export function applePay(): string {
+    return "apple-pay";
+}
---
+++ packages/payment-integration-core/src/lib/index.ts
@@ -0,0 +1,1 @@
+export * from './default-payment-integration-service'
