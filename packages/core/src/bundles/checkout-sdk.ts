6ccc459203ab470ae64e823005337a46974955e6
animesh1987 <animeshsingh1987@gmail.com>
2022-11-24 17:28:46 +1100

    chore(common): CHECKOUT-7058 Fix exports for embedded messenger

--- packages/core/src/bundles/checkout-sdk.ts
+++ packages/core/src/bundles/checkout-sdk.ts
@@ -1,8 +1,37 @@
 export { createTimeout } from '@bigcommerce/request-sender';
 
-export { createCheckoutService } from '../checkout';
+export { createCheckoutService, CheckoutService, CheckoutSelectors } from '../checkout';
 export { createCheckoutButtonInitializer } from '../checkout-buttons';
-export { embedCheckout } from '../embedded-checkout';
+export {
+    embedCheckout,
+    BodyStyles,
+    LinkStyles,
+    ButtonStyles,
+    TextInputStyles,
+    CheckableInputStyles,
+    LabelStyles,
+    ChecklistStyles,
+    LoadingIndicatorStyles,
+    StepStyles,
+    InlineElementStyles,
+    BlockElementStyles,
+    InputStyles,
+    EmbeddedCheckout,
+    EmbeddedCheckoutMessengerOptions,
+    EmbeddedCheckoutMessenger,
+    CustomError,
+    EmbeddedCheckoutStyles,
+    EmbeddedCheckoutOptions,
+    EmbeddedContentOptions,
+    EmbeddedCheckoutEventType,
+    EmbeddedCheckoutError,
+    EmbeddedCheckoutCompleteEvent,
+    EmbeddedCheckoutErrorEvent,
+    EmbeddedCheckoutFrameErrorEvent,
+    EmbeddedCheckoutFrameLoadedEvent,
+    EmbeddedCheckoutLoadedEvent,
+    EmbeddedCheckoutSignedOutEvent,
+ } from '../embedded-checkout';
 export { createEmbeddedCheckoutMessenger } from '../embedded-checkout/iframe-content';
 export { createLanguageService } from '../locale';
 export { createCurrencyService } from '../currency';
